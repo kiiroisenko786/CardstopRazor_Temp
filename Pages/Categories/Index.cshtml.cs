25acbf5024874d3bb6e9c5bc4ee6424350c992c0
Taahaa <58344496+JJosuke@users.noreply.github.com>
2023-09-19 03:39:32 +0100

    Category display needs restest with working sql server

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -1,3 +1,5 @@
+using CardstopRazor_Temp.Data;
+using CardstopRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,19 @@ namespace CardstopRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        // Create applicationdbcontext
+        private readonly ApplicationDbContext _db;
+        // Create category list
+        public List<Category> CategoryList { get; set; }
+        // Assign db to local variable to use
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public void OnGet()
         {
+            // Add all db categories to list and assign
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
--- obj/Debug/net8.0/CardstopRazor_Temp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CardstopRazor_Temp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardstopRazor_Temp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57e220489790b6d97ed317b1ffae482ce09df63d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1a9dce9e5297e579a3f15501e732500872b840")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardstopRazor_Temp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardstopRazor_Temp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
